428b31fdf6d19492d5d92c0a54cc847f722a3b62
wangzheng <zheng.wang@fih-foxconn.com>
2020-05-06 11:09:15 +0800

    modified the config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -6,17 +6,11 @@ module.exports = {
     lineNumbers: true,
   },
   locales: {
-    // 键名是该语言所属的子路径
-    // 作为特例，默认语言可以使用 '/' 作为其路径。
     '/': {
-      lang: 'en-US', // 将会被设置为 <html> 的 lang 属性
-      title: 'VuePress-wz',
-      description: 'wz-Vue-powered Static Site Generator',
+      lang: 'en-US',
     },
     '/zh/': {
       lang: 'zh-CN',
-      title: 'VuePress-王',
-      description: '王-Vue 驱动的静态网站生成器',
     },
   },
   themeConfig: {
@@ -28,7 +22,6 @@ module.exports = {
         label: 'English',
         selectText: 'Languages',
         ariaLabel: 'Select language',
-        editLinkText: 'Edit this page on GitHub',
         lastUpdated: 'Last Updated',
         nav: require('./nav/en.js'),
       },
@@ -36,12 +29,9 @@ module.exports = {
         label: '简体中文',
         selectText: '选择语言',
         ariaLabel: '选择语言',
-        editLinkText: '在 GitHub 上编辑此页',
         lastUpdated: '上次更新',
         nav: require('./nav/zh.js'),
       },
     },
-    // nav: require('./nav/en.js'),
-    // sidebar,
   },
 };
